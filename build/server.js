e6817bfed8b9710aa6bdc67c54942b58a375e85a
jessdev <gjfigueroah@gmail.com>
2020-11-09 12:41:42 -0500

    Correccion server.js

--- build/server.js
+++ build/server.js
@@ -8,13 +8,14 @@ const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const hbs_1 = __importDefault(require("hbs"));
 require("./hbs/helpers");
+const port = process.env.PORT || 3000;
 //EXPRESS HBS ENGINE
 hbs_1.default.registerPartials(__dirname + "/views/parciales");
 app.use(express_1.default.static(__dirname + '/public'));
 app.set('views', path_1.default.join(__dirname, '/views'));
 app.set('view engine', 'hbs');
-app.listen(8080, () => {
-    console.log("Escuchando peticiones el puerto 3000");
+app.listen(port, () => {
+    console.log("Escuchando peticiones el puerto: ", port);
 });
 app.get("/", function (req, res) {
     res.render('home', {
